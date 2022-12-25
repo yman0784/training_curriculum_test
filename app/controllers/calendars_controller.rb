4dfcbfed6d866c3dc8e7d86424bdfef22466dc33
yman0794 <tykesam@yahoo.co.jp>
2022-12-26 00:32:53 +0900

    リバート、ブランチ変更3

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -37,6 +37,5 @@ class CalendarsController < ApplicationController
       days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans}
       @week_days.push(days)
     end
-
   end
 end
